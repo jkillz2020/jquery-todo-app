6326259277771efd713f3054049f7079869599b0
jkillz2020 <kilgore.josh@gmail.com>
2016-10-30 23:26:29 -0500

    edit working

--- jquery-todo-app/javascripts/main.js
+++ jquery-todo-app/javascripts/main.js
@@ -2,18 +2,25 @@
 
 $(document).ready(function(){
   $("#clicky-button").on('click', ()=>{
-    let tasky = $('#todo-search').val();
+    let tasky = $('#todo-input').val();
     console.log("its working");
-    $('#output').append(`<div class="panel-body col-md-4">${tasky}<label><button class="btn btn-md delete-btn">Delete</button><button class="btn btn-md edit-btn">Edit</button><input type="checkbox" id="cbox1" value="first_checkbox"> Complete?</label><br></div>`);
+    $('#output').append(`<div class="col-md-4"><span>${tasky}</span><button class="btn btn-md btn-space delete-btn">Delete</button><button class="btn btn-md btn-space edit-btn">Edit</button><input class="btn-space" type="checkbox" id="cbox1" value="first_checkbox"> Complete?<br></div>`);
   });
   $(document).on('click', '.delete-btn',function(){
     $(this).closest('div').remove();
   });
-  $(document).on('click', '.edit-btn',function(){
-    $(this).closest('div').remove();
+  // $(document).on('click', '.edit-btn',function(){
+  //   $(this).closest('div').html(this);
+  // });
+  $(document).on('click', '.edit-btn', function(){
+    $("#todo-input").focus();
+    $("#todo-input").val("");
+    let editItem = $("#todo-input").val();
+    console.log($(this).siblings('span').text(editItem));
   });
 
-  // $('#checkbox').change(function(){
-  //     var c = this.checked ? '#complete-list'.innerHTML;
-  // });
+  $('#checkbox').change(function(){
+       var c = this.checked ? '#output'.append() : '#completed'.append();
+       console.log(this);
+  });
 });
