0b1aa52576ddeb0e2282c3e548412393e079ac20
jkillz2020 <kilgore.josh@gmail.com>
2016-10-31 19:42:51 -0500

    getting array of objects from firebase

--- jquery-todo-app/javascripts/main.js
+++ jquery-todo-app/javascripts/main.js
@@ -1,11 +1,16 @@
 "use strict";
 
+let apiKeys={};
+
 $(document).ready(function(){
   FbAPI.firebaseCredentials().then(function(keys){
     console.log("keys", keys);
+    apiKeys=keys;
+    firebase.initializeApp(apiKeys);
+    FbAPI.getTodos(apiKeys).then(function(items){
+      console.log("items from Firebase", items);
+    });
   });
-
-
 
   });
   
