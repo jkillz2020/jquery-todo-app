a8b7f49c96761f5ffbbdf09278aa4c0abe33d1a5
jkillz2020 <kilgore.josh@gmail.com>
2016-11-01 19:15:43 -0500

    edit button working

--- jquery-todo-app/javascripts/firebaseMethods.js
+++ jquery-todo-app/javascripts/firebaseMethods.js
@@ -21,7 +21,51 @@ var FbAPI =(function(oldFirebase){
     });
   };
 
+  oldFirebase.addTodo = function(apiKeys, newItem){
+    return new Promise((resolve, reject)=>{
+      $.ajax({
+        method: "POST",
+        url: `${apiKeys.databaseURL}/items.json`,
+        data: JSON.stringify(newItem),
+        dataType: 'json'
+      }).then((response)=>{
+        console.log("response from POST", response);
+        resolve(response);
+      }, (error)=>{
+        reject(error);
+      });
+    });
+  };
 
+  oldFirebase.deleteTodo = function(apiKeys, itemId){ 
+    return new Promise((resolve, reject)=>{
+      $.ajax({
+        method: "DELETE",
+        url: `${apiKeys.databaseURL}/items/${itemId}.json`,
+      }).then((response)=>{
+        console.log("response from DELETE", response);
+        resolve(response);
+      }, (error)=>{
+        reject(error);
+      });
+    });
+  };
+
+  oldFirebase.editTodo = function(apiKeys, itemId, editedItem){
+    return new Promise((resolve, reject)=>{
+      $.ajax({
+        method: "PUT",
+        url: `${apiKeys.databaseURL}/items/${itemId}.json`,
+        data: JSON.stringify(editedItem),
+        dataType: 'json'
+      }).then((response)=>{
+        console.log("response from PUT", response);
+        resolve(response);
+      }, (error)=>{
+        reject(error);
+      });
+    });
+  };
 
 
 
--- jquery-todo-app/javascripts/main.js
+++ jquery-todo-app/javascripts/main.js
@@ -2,17 +2,81 @@
 
 let apiKeys={};
 
+function putTodoInDOM (){
+    FbAPI.getTodos(apiKeys).then(function(items){
+      console.log("items from Firebase", items);
+      $('#completed-tasks').html("");
+      $('#incomplete-tasks').html("");
+      items.forEach(function(item){
+        if(item.isCompleted === true){
+        let newListItem = '<li>';
+        newListItem+='<div class="col-xs-8">';
+        newListItem+='<input class="checkboxStyle" type="checkbox" checked>';
+        newListItem+=`<label class="inputLabel">${item.task}</label>`;
+        newListItem+='<input type="text" class="inputTask">';
+        //apend to list
+        $('#completed-tasks').append(newListItem);
+      }else{
+        let newListItem = '<li>';
+        newListItem+='<div class="col-xs-8">';
+        newListItem+='<input class="checkboxStyle" type="checkbox">';
+        newListItem+=`<label class="inputLabel">${item.task}</label>`;
+        newListItem+='<input type="text" class="inputTask">';
+        newListItem+='</div>';
+        newListItem+='<div class="col-xs-4">';
+        newListItem+=`<button class="btn btn-default col-xs-6 edit"  data-fbid="${item.id}">Edit</button> `;
+        newListItem+=`<button class="btn btn-danger col-xs-6 delete"  data-fbid="${item.id}">Delete</button> `;
+        newListItem+='</div>';
+        newListItem+='</li>'; 
+        $('#incomplete-tasks').append(newListItem);
+      }
+    });
+    });
+}
+
 $(document).ready(function(){
   FbAPI.firebaseCredentials().then(function(keys){
     console.log("keys", keys);
     apiKeys=keys;
     firebase.initializeApp(apiKeys);
-    FbAPI.getTodos(apiKeys).then(function(items){
-      console.log("items from Firebase", items);
+    putTodoInDOM();
+  });
+
+  $('#clicky-button').on('click', function(){
+    let newItem = {
+      "task": $("#todo-input").val(),
+      "isCompleted": false
+    };
+    FbAPI.addTodo(apiKeys, newItem).then(function(){
+      putTodoInDOM();
     });
   });
 
+  $("ul").on("click", ".delete", function(){
+    let itemId = $(this).data("fbid");
+    FbAPI.deleteTodo(apiKeys, itemId).then(function(){
+      putTodoInDOM();
+    });
   });
+
+  $("ul").on("click", ".edit", function(){
+    let itemId = $(this).data("fbid");
+    let parent = $(this).closest("li");
+    if(!parent.hasClass("editMode")){
+      parent.addClass("editMode");
+    } else {
+    let editedItem = {
+      "task": parent.find(".inputTask").val(),
+      "isCompleted": false
+    };
+    FbAPI.editTodo(apiKeys, itemId, editedItem).then(function(response){
+      parent.removeClass("editMode");
+      putTodoInDOM();
+    });
+      //firebase stuff
+    }
+  });
+});
   
 
 
